6387a261d61d4b9dcd252ee995ca2c196701bcc3
Syazanie Amirin <51242108+SyazanieAmirin@users.noreply.github.com>
2021-09-15 04:09:50 +0800

    Added mobile menu

--- JavaScript/home.js
+++ JavaScript/home.js
@@ -1,9 +1,17 @@
+// For toggling mobile menu
 function toggleMobileMenu() {
-    var item = document.getElementById("sidebar-items-container");
+    var item = document.getElementById("mobile-items-container-navbar");
+    const addCSS = s => document.head.appendChild(document.createElement("style")).innerHTML=s;
 
     if (item.style.display === "none") {
-        item.style.display = "block";
+        item.style.display = "flex";
+
+        // Add overflow-y as hidden so user can't scroll while the mobile menu is open
+        addCSS("body{overflow-y: hidden;}")
     } else {
         item.style.display = "none";
+        
+        // Add overflow-y as scroll so the user can scroll once the menu has been closed 
+        addCSS("body{overflow-y: scroll;}")
     }
 }
